cf274c475e98dfc525383e3c8a7a8796c1df7eff
pascal Christoph <christoph@hbz-nrw.de>
2013-05-13 14:00:36 +0200

    Rename "resource" to "manifestation"
    - add rdf:seeAlso and rdf:type (this closes issue 13)
    - update license in metadata.ttl

--- datasets/de.dbpedia.org/lobid.org/resource/scripts/makeLinks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# get the data from datahub.io and transform it to the expected links
-
-rapper -i turtle http://lobid.org/download/dumps/DE-605/enrich/2de-dbpedia.ttl | perl -pe 's|(<.*?>).*(<.*?>).*|\2 <http://rdvocab.info/RDARelationshipsWEMI/manifestationOfWork> \1 .|' | grep '^<http://de.dbpedia.org/'
